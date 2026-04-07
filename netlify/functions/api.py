158e0daf85e01cd0c67df282966f85a6a9a877ed
Cristianblacksip <168103666+Cristianblacksip@users.noreply.github.com>
2026-04-06 22:04:28 -0500

    Fix: Ajuste de rutas en api.py y netlify.toml tras mover archivos

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,15 +1,14 @@
 import sys
 import os
 
-# Añadir el directorio de la app Flask al path de Python
+# Ahora app.py está solo dos niveles arriba, no tres.
 sys.path.insert(
-    0,
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'proyecto_comfandi')
+    0, 
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
 )
 
 from serverless_wsgi import handle_request
-import app as flask_module
-
+import app as flask_module # Esto buscará app.py en la raíz
 
 def handler(event, context):
     return handle_request(flask_module.app, event, context, binary_support=True)
